00d5cba755aadee8e5c0234f46b151950057acfa
JZims <zimmerman.j@hotmail.com>
2021-08-10 19:56:45 -0400

    One solution found

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,5 +1,28 @@
 function selectionSort(arr) {
-  // type your code here
+
+  //Iterate through initially
+  for (var i = 0; i < arr.length; i++){
+    //Store the INDEX OF the first element as the smallest value seen so far
+    let smallestNum = i
+    for(var j = i + 1; j < arr.length; j++ ){
+    //Compare this item to the next item in the array until you find a smaller number
+    
+        if (arr[j] < arr[smallestNum]) smallestNum = j
+    //Save the index of where the value is found as the new lowest number when found
+    
+    }
+    //If the minimum is not the value you initially began with, swap the two values
+    //Repeat this with the next item until the array is sorted
+      if(i !== smallestNum){
+        var temp = arr[i]
+        arr[i] = arr[smallestNum]
+        arr[smallestNum] = temp
+    }
+    
+  }
+
+  return arr
+
 }
 
 if (require.main === module) {
@@ -21,3 +44,10 @@ module.exports = selectionSort;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+  
+  
+  
+
+ 
